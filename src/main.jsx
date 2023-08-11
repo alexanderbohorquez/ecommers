9973d45667fda2ade9a072adc9477521bbb7de99
alexanderbohorquez <alexanderbohorquez96@gmail.com>
2023-08-10 20:04:41 -0500

    hola

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootswatch/dist/Journal/bootstrap.min.css";
 import './index.css'
 
